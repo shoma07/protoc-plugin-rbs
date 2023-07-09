87062e82019c55ff102b5c580981bc8717a985c5
shoma07 <23730734+shoma07@users.noreply.github.com>
2023-07-09 14:38:39 +0900

    initial commit

---
+++ lib/protoc/plugin/rbs.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "rbs/version"
+
+module Protoc
+  module Plugin
+    module Rbs
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
